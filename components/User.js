83385b317860622689ba418451d476b8785d3375
dong-geon-Lee <ch9716@naver.com>
2022-07-02 15:44:09 +0900

    add authentication and get user data from google api

--- components/User.js
+++ components/User.js
@@ -1,3 +1,29 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function User() {
-  return <div>User</div>;
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        <img
+          src={session.user.image}
+          alt="user-image"
+          onClick={signOut}
+          className="h-10 w-10 rounded-full hover:bg-gray-200 cursor-pointer p-1"
+        ></img>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <button
+        onClick={signIn}
+        className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md"
+      >
+        Sign in
+      </button>
+    </>
+  );
 }
