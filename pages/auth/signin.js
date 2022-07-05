a97b12ab9fdead995f6d9c48a051f7659b469d96
dong-geon-Lee <ch9716@naver.com>
2022-07-05 16:12:39 +0900

    test branch setting

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,40 +1,5 @@
 import React from "react";
-import Header from "../../components/Header";
-import { getProviders, signIn } from "next-auth/react";
 
-export default function signin({ providers }) {
-  return (
-    <div>
-      <Header />
-
-      <div className="mt-40">
-        {Object.values(providers).map((p) => (
-          <div key={p.name} className="flex flex-col items-center">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
-              alt="google-logo"
-              className="w-52 object-cover"
-            />
-            <p className="text-sm italic my-10 text-center">
-              This website is created for learning purposes
-            </p>
-            <button
-              onClick={() => signIn(p.id, { callbackUrl: "/" })}
-              className="bg-red-400 rounded-lg text-white p-3 hover:bg-red-500"
-            >
-              Sign in with {p.name}
-            </button>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
-
-export async function getServerSideProps() {
-  const providers = await getProviders();
-
-  return {
-    props: { providers },
-  };
+export default function signin() {
+  return <div></div>;
 }
--- pages/search.js
+++ pages/search.js
@@ -1,17 +1,3 @@
-import Head from "next/head";
-import SearchHeader from "../components/SearchHeader";
-
 export default function search() {
-  return (
-    <div>
-      <Head>
-        <title>Search Page</title>
-      </Head>
-
-      {/* Search Header */}
-      <SearchHeader />
-
-      {/* Search Results */}
-    </div>
-  );
+  return <div></div>;
 }
