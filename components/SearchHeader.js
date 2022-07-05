d05f1d0e4b1a84e2208b2048f16d2ce27f38e86a
dong-geon-Lee <ch9716@naver.com>
2022-07-05 15:53:53 +0900

    create search header option

--- components/SearchHeader.js
+++ components/SearchHeader.js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { useRef } from "react";
 import { MicrophoneIcon, SearchIcon, XIcon } from "@heroicons/react/solid";
 import User from "./User";
+import SearchHeaderOptions from "./SearchHeaderOptions";
 
 export default function SearchHeader() {
   const router = useRouter();
@@ -15,7 +16,7 @@ export default function SearchHeader() {
 
     if (!term.trim()) return;
 
-    router.push(`/search?term=${term.trim()}`);
+    router.push(`/search?term=${term.trim()}&searchType=`);
   };
 
   return (
@@ -48,6 +49,8 @@ export default function SearchHeader() {
 
         <User className="ml-auto whitespace-nowrap" />
       </div>
+
+      <SearchHeaderOptions />
     </header>
   );
 }
