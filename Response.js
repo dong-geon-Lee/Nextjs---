56eed4837982db5e4e7f54b6d9194edee5541671
dong-geon-Lee <ch9716@naver.com>
2022-07-07 13:16:13 +0900

    get data from google api and create mock data

---
+++ Response.js
@@ -0,0 +1,433 @@
+export default {
+  kind: "customsearch#search",
+  url: {
+    type: "application/json",
+    template:
+      "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json",
+  },
+  queries: {
+    request: [
+      {
+        title: "Google Custom Search - 이순신",
+        totalResults: "8290000",
+        searchTerms: "이순신",
+        count: 10,
+        startIndex: 1,
+        inputEncoding: "utf8",
+        outputEncoding: "utf8",
+        safe: "off",
+        cx: "1cab03cc7728e346f",
+      },
+    ],
+    nextPage: [
+      {
+        title: "Google Custom Search - 이순신",
+        totalResults: "8290000",
+        searchTerms: "이순신",
+        count: 10,
+        startIndex: 11,
+        inputEncoding: "utf8",
+        outputEncoding: "utf8",
+        safe: "off",
+        cx: "1cab03cc7728e346f",
+      },
+    ],
+  },
+  context: {
+    title: "go-v3",
+  },
+  searchInformation: {
+    searchTime: 0.397719,
+    formattedSearchTime: "0.40",
+    totalResults: "8290000",
+    formattedTotalResults: "8,290,000",
+  },
+  items: [
+    {
+      kind: "customsearch#result",
+      title: "이순신 - 나무위키",
+      htmlTitle: "<b>이순신</b> - 나무위키",
+      link: "https://namu.wiki/w/%EC%9D%B4%EC%88%9C%EC%8B%A0",
+      displayLink: "namu.wiki",
+      snippet:
+        "16세기 조선의 무신으로, 임진왜란 및 정유재란 당시 조선 수군을 통솔했던 제독이다. 오늘날 한국에서는 대표적인 구국영웅으로 여겨지고 있다.",
+      htmlSnippet:
+        "16세기 조선의 무신으로, 임진왜란 및 정유재란 당시 조선 수군을 통솔했던 제독이다. 오늘날 한국에서는 대표적인 구국영웅으로 여겨지고 있다.",
+      formattedUrl: "https://namu.wiki/w/이순신",
+      htmlFormattedUrl: "https://namu.wiki/w/<b>이순신</b>",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwN0hmg-lcE1gfpINnVoG7Ozy3AzoWkMLBNG9GNPuzY6qLJiu5n-Vkz-Y",
+            width: "225",
+            height: "225",
+          },
+        ],
+        metatags: [
+          {
+            "application-name": "나무위키",
+            "msapplication-starturl":
+              "/w/%EB%82%98%EB%AC%B4%EC%9C%84%ED%82%A4:%EB%8C%80%EB%AC%B8",
+            "theme-color": "#008275",
+            viewport:
+              "user-scalable=no, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0, width=device-width",
+            "msapplication-tooltip": "나무위키",
+            "mobile-web-app-capable": "yes",
+          },
+        ],
+        cse_image: [
+          {
+            src: "x-raw-image:///6135ac3e89e2f91c75a1bcb54c5586a3fd9cc955fbc1f47a025326d75905a3a6",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "이순신 - 위키백과, 우리 모두의 백과사전",
+      htmlTitle: "<b>이순신</b> - 위키백과, 우리 모두의 백과사전",
+      link: "https://ko.wikipedia.org/wiki/%EC%9D%B4%EC%88%9C%EC%8B%A0",
+      displayLink: "ko.wikipedia.org",
+      snippet:
+        "이순신(李舜臣, 1545년 4월 28일 (음력 3월 8일) ~ 1598년 12월 16일 (음력 11월 19일))은 조선 중기의 무신이었다. 본관은 덕수(德水), 자는 여해(汝諧), ...",
+      htmlSnippet:
+        "<b>이순신</b>(李舜臣, 1545년 4월 28일 (음력 3월 8일) ~ 1598년 12월 16일 (음력 11월 19일))은 조선 중기의 무신이었다. 본관은 덕수(德水), 자는 여해(汝諧),&nbsp;...",
+      cacheId: "WRFAOAaSkdMJ",
+      formattedUrl: "https://ko.wikipedia.org/wiki/이순신",
+      htmlFormattedUrl: "https://ko.wikipedia.org/wiki/<b>이순신</b>",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS11qyEwtiwfY4_6Noe_1qqL34gU9KiZc5E3FvvQ8uqWMXNgOeZ3bAzPo8",
+            width: "196",
+            height: "257",
+          },
+        ],
+        metatags: [
+          {
+            referrer: "origin",
+            "og:image":
+              "https://upload.wikimedia.org/wikipedia/commons/e/e9/%EC%9D%B4%EC%88%9C%EC%8B%A0.jpg",
+            "theme-color": "#eaecf0",
+            "og:image:width": "1200",
+            "og:type": "website",
+            viewport:
+              "width=device-width, initial-scale=1.0, user-scalable=yes, minimum-scale=0.25, maximum-scale=5.0",
+            "og:title": "이순신 - 위키백과, 우리 모두의 백과사전",
+            "og:image:height": "1571",
+            "format-detection": "telephone=no",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://upload.wikimedia.org/wikipedia/commons/e/e9/%EC%9D%B4%EC%88%9C%EC%8B%A0.jpg",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "이순신리더십 국제센터",
+      htmlTitle: "<b>이순신</b>리더십 국제센터",
+      link: "https://www.changwon.go.kr/yisunsin/",
+      displayLink: "www.changwon.go.kr",
+      snippet:
+        "'K-리더십' 오직 이순신 리더십 국제센터에서 경험할 수 있습니다. '역사·힐링·어드벤처·예술' 상상하는 모든 것이 이루어집니다.",
+      htmlSnippet:
+        "&#39;K-리더십&#39; 오직 <b>이순신</b> 리더십 국제센터에서 경험할 수 있습니다. &#39;역사·힐링·어드벤처·예술&#39; 상상하는 모든 것이 이루어집니다.",
+      cacheId: "BhA9Q7yUPzgJ",
+      formattedUrl: "https://www.changwon.go.kr/yisunsin/",
+      htmlFormattedUrl: "https://www.changwon.go.kr/yisunsin/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQwzDTjvu0-Ke98uQdB4-5DUAOcBCwEOui4Lun-_iIzHcew5PkJHnmgDF0",
+            width: "225",
+            height: "225",
+          },
+        ],
+        metatags: [
+          {
+            viewport:
+              "width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=2.0,user-scalable=no",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://www.changwon.go.kr/yisunsin/images/popup_1.jpg",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "이순신 - 재미한국학교협의회 (NAKS)",
+      htmlTitle: "<b>이순신</b> - 재미한국학교협의회 (NAKS)",
+      link: "https://www.naks.org/jml/writing-competitions-menu/soonshin-lee-competition-menu",
+      displayLink: "www.naks.org",
+      snippet:
+        "제3회 충무공 이순신 글쓰기 공모-읽기 자료 파일, 10/14/2019. 제1회 충무공이순신 글쓰기 대회 수상작집, 04/12/2018. 제1회 충무공 이순신 글쓰기 대회 최우수상 ...",
+      htmlSnippet:
+        "제3회 충무공 <b>이순신</b> 글쓰기 공모-읽기 자료 파일, 10/14/2019. 제1회 충무공<b>이순신</b> 글쓰기 대회 수상작집, 04/12/2018. 제1회 충무공 <b>이순신</b> 글쓰기 대회 최우수상&nbsp;...",
+      cacheId: "-OoOxlWCjJIJ",
+      formattedUrl:
+        "https://www.naks.org/jml/writing...menu/soonshin-lee-competition-menu",
+      htmlFormattedUrl:
+        "https://www.naks.org/jml/writing...menu/soonshin-lee-competition-menu",
+      pagemap: {
+        metatags: [
+          {
+            "msapplication-tilecolor": "#da532c",
+            "theme-color": "#ffffff",
+            handheldfriendly: "true",
+            viewport:
+              "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+            "apple-mobile-web-app-capable": "YES",
+            rights:
+              "©2007-2021 The National Association for Korean Schools (NAKS). All rights reserved.",
+          },
+        ],
+        sitenavigationelement: [
+          {
+            name: "Home NAKS 일정표 NAKS 공지 및 소식 창립 40주년 기념사업 역사서 축하메세지 동영상 역사 동영상 합창 프로젝트 동영상",
+            url: "Home",
+          },
+          {
+            name: "NAKS 일정표",
+            url: "NAKS 일정표",
+          },
+          {
+            name: "역사서",
+            url: "역사서",
+          },
+          {
+            name: "총회장 인사말",
+            url: "총회장 인사말",
+          },
+          {
+            name: "나의 꿈 말하기 대회",
+            url: "나의 꿈 말하기 대회",
+          },
+          {
+            name: "독서 감상문 쓰기대회",
+            url: "독서 감상문 쓰기대회",
+          },
+          {
+            name: "서재필",
+            url: "서재필",
+          },
+          {
+            name: "역대 학술대회 개최지",
+            url: "역대 학술대회 개최지",
+          },
+          {
+            name: "장기근속 교사",
+            url: "장기근속 교사",
+          },
+          {
+            name: "NAKS 한국어 능력평가(NKT)",
+            url: "NAKS 한국어 능력평가(NKT)",
+          },
+          {
+            name: "한국역사 문화 리더십 캠프",
+            url: "한국역사 문화 리더십 캠프",
+          },
+          {
+            name: "친구야 대한민국을 소개할게",
+            url: "친구야 대한민국을 소개할게",
+          },
+          {
+            name: "학술대회 프로그램 강의록",
+            url: "학술대회",
+          },
+          {
+            name: "프로그램",
+            url: "프로그램",
+          },
+          {
+            name: "회보",
+            url: "회보",
+          },
+          {
+            name: "한국어",
+            url: "한국어",
+          },
+          {
+            name: "Korean Spelling Bee",
+            url: "Korean Spelling Bee",
+          },
+          {
+            name: "NAKS_재외동포를 위한 한국어 1-1",
+            url: "NAKS_재외동포를 위한 한국어 1-1",
+          },
+          {
+            name: "NAKS 행사 사진",
+            url: "NAKS 행사 사진",
+          },
+          {
+            name: "낙스 이메일 신청",
+            url: "낙스 이메일 신청",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "아산성웅이순신축제 | 아산시 문화관광",
+      htmlTitle: "아산성웅<b>이순신</b>축제 | 아산시 문화관광",
+      link: "https://www.asan.go.kr/tour/design/node/?menu=010401",
+      displayLink: "www.asan.go.kr",
+      snippet:
+        "“아산성웅이순신축제”는 매년 충무공 이순신 장군의 탄신일인 4월 28일을 전후하여 충무공의 애국애족 정신을 고취하고 국난극복의 위엄을 계승하고자 열리는 충남 ...",
+      htmlSnippet:
+        "“아산성웅<b>이순신</b>축제”는 매년 충무공 <b>이순신</b> 장군의 탄신일인 4월 28일을 전후하여 충무공의 애국애족 정신을 고취하고 국난극복의 위엄을 계승하고자 열리는 충남&nbsp;...",
+      cacheId: "_2GikAL9Va0J",
+      formattedUrl: "https://www.asan.go.kr/tour/design/node/?menu=010401",
+      htmlFormattedUrl: "https://www.asan.go.kr/tour/design/node/?menu=010401",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSWYQpbJidtbs_4bO_OhGQnZZIzNn6RRZEhAUGrN2XmUDVfLHy--xDU_O7v",
+            width: "368",
+            height: "137",
+          },
+        ],
+        metatags: [
+          {
+            "og:type": "웹사이트",
+            viewport: "width=device-width, initial-scale=1",
+            "og:title": "아산시 문화관광",
+            "og:url": "https://www.asan.go.kr/tour/",
+            "og:description": "너와 나 함께하는 아산에서 여행",
+            "format-detection": "telephone=no",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://www.asan.go.kr/tour/design/img/content/010401_visual.jpg",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "충무공 이순신",
+      htmlTitle: "충무공 <b>이순신</b>",
+      link: "https://www.gyeongnam.go.kr/yisunshin/index.gyeong",
+      displayLink: "www.gyeongnam.go.kr",
+      snippet:
+        "충무공이순신 ; 7년간의 전쟁! 임진왜란. 배경/원인 ; 세계 최고의 돌격선! 거북선. 거북선 ; 모든 것을 내려놓다! 백의종군로. 백의종군의 의의 ...",
+      htmlSnippet:
+        "충무공<b>이순신</b> ; 7년간의 전쟁! 임진왜란. 배경/원인 ; 세계 최고의 돌격선! 거북선. 거북선 ; 모든 것을 내려놓다! 백의종군로. 백의종군의 의의&nbsp;...",
+      cacheId: "RNqdrRz0ogMJ",
+      formattedUrl: "https://www.gyeongnam.go.kr/yisunshin/index.gyeong",
+      htmlFormattedUrl: "https://www.gyeongnam.go.kr/yisunshin/index.gyeong",
+      pagemap: {
+        metatags: [
+          {
+            viewport:
+              "width=device-width,initial-scale=1.0,user-scalable=yes,maximum-scale=10",
+            title: "충무공 이순신",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "#이순신 hashtag on Instagram • Photos and Videos",
+      htmlTitle: "#<b>이순신</b> hashtag on Instagram • Photos and Videos",
+      link: "https://www.instagram.com/explore/tags/%EC%9D%B4%EC%88%9C%EC%8B%A0/",
+      displayLink: "www.instagram.com",
+      snippet:
+        "43k Posts - See Instagram photos and videos from '이순신' hashtag.",
+      htmlSnippet:
+        "43k Posts - See Instagram photos and videos from &#39;<b>이순신</b>&#39; hashtag.",
+      formattedUrl: "https://www.instagram.com/explore/tags/이순신/",
+      htmlFormattedUrl: "https://www.instagram.com/explore/tags/<b>이순신</b>/",
+      pagemap: {
+        metatags: [
+          {
+            "theme-color": "#ffffff",
+            "apple-mobile-web-app-status-bar-style": "default",
+            viewport:
+              "width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover",
+            "mobile-web-app-capable": "yes",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "충무공 이순신",
+      htmlTitle: "충무공 <b>이순신</b>",
+      link: "https://www.gyeongnam.go.kr/index.gyeong?menuCd=DOM_000009307003003004",
+      displayLink: "www.gyeongnam.go.kr",
+      snippet:
+        "노량해전 이전의 상황. 연전연승을 이끌었던 이순신 장군이 삼도수군통제사에서 백의종군하기까지의 과정과 칠천량 해전의 패배로 괴멸될 위기에 처한 조선 수군을 ...",
+      htmlSnippet:
+        "노량해전 이전의 상황. 연전연승을 이끌었던 <b>이순신</b> 장군이 삼도수군통제사에서 백의종군하기까지의 과정과 칠천량 해전의 패배로 괴멸될 위기에 처한 조선 수군을&nbsp;...",
+      cacheId: "dLcLKf1pns8J",
+      formattedUrl: "https://www.gyeongnam.go.kr/index.gyeong?menuCd=DOM...",
+      htmlFormattedUrl:
+        "https://www.gyeongnam.go.kr/index.gyeong?menuCd=DOM...",
+      pagemap: {
+        metatags: [
+          {
+            viewport:
+              "width=device-width,initial-scale=1.0,user-scalable=yes,maximum-scale=10",
+            title: "충무공 이순신",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "21C 이순신 오류시정 프로젝트",
+      htmlTitle: "21C <b>이순신</b> 오류시정 프로젝트",
+      link: "http://korea.prkorea.com/",
+      displayLink: "korea.prkorea.com",
+      snippet:
+        "주 한국 프랑스 대사관 French Embassy in Korea http://www.ambafrance-kr.org/spip.php?article38 · 나도 21c 이순신 :: 누구나 불명의 영웅! · 나도 21c 이순신 :: 누구나 ...",
+      htmlSnippet:
+        "주 한국 프랑스 대사관 French Embassy in Korea http://www.ambafrance-kr.org/spip.php?article38 &middot; 나도 21c <b>이순신</b> :: 누구나 불명의 영웅! &middot; 나도 21c <b>이순신</b> :: 누구나&nbsp;...",
+      cacheId: "hBds0o2oT-AJ",
+      formattedUrl: "korea.prkorea.com/",
+      htmlFormattedUrl: "korea.prkorea.com/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfj1eSkRCTQxK0JSOdioNn0arzl4LVqyezj2KsappcjsY76khru1vRnZc",
+            width: "333",
+            height: "151",
+          },
+        ],
+        cse_image: [
+          {
+            src: "http://korea.prkorea.com/kor/img/slide_img_02.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "이순신축제",
+      htmlTitle: "<b>이순신</b>축제",
+      link: "https://culture.asan.go.kr/_esunshin/",
+      displayLink: "culture.asan.go.kr",
+      snippet:
+        "“아산성웅이순신축제”는 매년 충무공 이순신 장군의 탄신일인 4월 28일을 전후하여 충무공의 애국애족 정신을 고취하고 국난극복의 위엄을 계승하고자 열리는 충남 ...",
+      htmlSnippet:
+        "“아산성웅<b>이순신</b>축제”는 매년 충무공 <b>이순신</b> 장군의 탄신일인 4월 28일을 전후하여 충무공의 애국애족 정신을 고취하고 국난극복의 위엄을 계승하고자 열리는 충남&nbsp;...",
+      cacheId: "0Hn1wVNOTgsJ",
+      formattedUrl: "https://culture.asan.go.kr/_esunshin/",
+      htmlFormattedUrl: "https://culture.asan.go.kr/_esunshin/",
+      pagemap: {
+        metatags: [
+          {
+            viewport: "width=device-width, initial-scale=1.0",
+          },
+        ],
+      },
+    },
+  ],
+};
